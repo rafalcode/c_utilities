5941d1f1fc3450a59491aff5caa36a5da359e7f0
rf73 <Ramon Fallon>
2019-01-18 14:40:46 +0000

    i've decided to do some postprocessing on the sequence. This new prog pare3 now works, is vg-good, though still a few thigns missing

---
+++ pare3.h
@@ -0,0 +1,34 @@
+/* it's all too easy to start over-complicating this: for example quotations. Here you woul dneed to check the last 2 characters of everyword, not just the last one, i.e "stop!", that adds new layers. */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#ifdef DBG
+#define CBUF 2
+#define WABUF 1
+#define LBUF 2
+#else
+#define CBUF 12
+#define WABUF 20
+#define LBUF 32
+#endif
+
+typedef unsigned char boole;
+typedef struct /* word type */
+{
+    char *w;
+    unsigned lp1; /* length of word plus one (i.e. includes null char */
+} w_c;
+
+typedef struct /* aw_c: array of words container */
+{
+    w_c **aw;
+    unsigned ab;
+    unsigned al;
+} aw_c;
+
+typedef struct /* aaw_c: array of array of words container */
+{
+    size_t numl; /* number of lines, i.e. rows */
+    aw_c **aaw; /* an array of pointers to aw_c */
+} aaw_c;
