23bcdad353aa365208dbc8f5e766b727ded41763
nutria <nutria@aspr3.home>
2019-01-19 22:39:24 +0000

    yeekrye ... working vg-good!

---
+++ morel.c
@@ -0,0 +1,248 @@
+/* modification of matread but operating on words instead of floats */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "onel.h"
+
+// proto ahoy
+aaw_c *processinpf(char *fname);
+
+typedef enum /* b_t for debugging, for seeing the branch we're in */
+{
+    BX, /* branch X */
+    BY, /* branch X */
+    BZ /* branch X */
+} b_t;
+
+w_c *crea_wc(unsigned initsz)
+{
+    w_c *wc=malloc(sizeof(w_c));
+    wc->lp1=initsz;
+    wc->w=malloc(wc->lp1*sizeof(char));
+    return wc;
+}
+
+void reall_wc(w_c **wc, unsigned *cbuf)
+{
+    w_c *twc=*wc;
+    unsigned tcbuf=*cbuf;
+    tcbuf += CBUF;
+    twc->lp1=tcbuf;
+    twc->w=realloc(twc->w, tcbuf*sizeof(char));
+    *wc=twc; /* realloc can often change the ptr */
+    *cbuf=tcbuf;
+    return;
+}
+
+void norm_wc(w_c **wc)
+{
+    w_c *twc=*wc;
+    twc->w=realloc(twc->w, twc->lp1*sizeof(char));
+    *wc=twc; /* realloc can often change the ptr */
+    return;
+}
+
+void free_wc(w_c **wc)
+{
+    w_c *twc=*wc;
+    free(twc->w);
+    free(twc);
+    return;
+}
+
+aw_c *crea_awc(unsigned initsz)
+{
+    int i;
+    aw_c *awc=malloc(sizeof(aw_c));
+    awc->ab=initsz;
+    awc->al=awc->ab;
+    awc->aw=malloc(awc->ab*sizeof(w_c*));
+    for(i=0;i<awc->ab;++i) 
+        awc->aw[i]=crea_wc(CBUF);
+    return awc;
+}
+
+void reall_awc(aw_c **awc, unsigned buf)
+{
+    int i;
+    aw_c *tawc=*awc;
+    tawc->ab += buf;
+    tawc->al=tawc->ab;
+    tawc->aw=realloc(tawc->aw, tawc->ab*sizeof(aw_c*));
+    for(i=tawc->ab-buf;i<tawc->ab;++i)
+        tawc->aw[i]=crea_wc(CBUF);
+    *awc=tawc;
+    return;
+}
+
+void norm_awc(aw_c **awc)
+{
+    int i;
+    aw_c *tawc=*awc;
+    /* free the individual w_c's */
+    for(i=tawc->al;i<tawc->ab;++i) 
+        free_wc(tawc->aw+i);
+    /* now release the pointers to those freed w_c's */
+    tawc->aw=realloc(tawc->aw, tawc->al*sizeof(aw_c*));
+    *awc=tawc;
+    return;
+}
+
+void free_awc(aw_c **awc)
+{
+    int i;
+    aw_c *tawc=*awc;
+    for(i=0;i<tawc->al;++i) 
+        free_wc(tawc->aw+i);
+    free(tawc->aw); /* unbelieveable: I left this out, couldn't find where I leaking the memory! */
+    free(tawc);
+    return;
+}
+
+aaw_c *crea_aawc(unsigned initsz)
+{
+    int i;
+    unsigned lbuf=initsz;
+    aaw_c *aawc=malloc(sizeof(aaw_c));
+    aawc->numl=0;
+    aawc->aaw=malloc(lbuf*sizeof(aw_c*));
+    for(i=0;i<initsz;++i) 
+        aawc->aaw[i]=crea_awc(WABUF);
+    return aawc;
+}
+
+void free_aawc(aaw_c **aw)
+{
+    int i;
+    aaw_c *taw=*aw;
+    for(i=0;i<taw->numl;++i) /* tried to release 1 more, no go */
+        free_awc(taw->aaw+i);
+    free(taw->aaw);
+    free(taw);
+}
+
+void prtaawcplain(aaw_c *aawc) /* print line and word details, but not the words themselves */
+{
+    int i, j, k;
+    for(i=0;i<aawc->numl;++i) {
+        printf("L%u#W%u): ", i, aawc->aaw[i]->al); 
+        for(j=0;j<aawc->aaw[i]->al;++j) {
+            printf("w%il%i:", j, aawc->aaw[i]->aw[j]->lp1-1);
+            for(k=0;k<aawc->aaw[i]->aw[j]->lp1-1;++k)
+                putchar(aawc->aaw[i]->aw[j]->w[k]);
+            putchar(' ');
+        }
+        putchar('\n');
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    /* argument accounting */
+    if(argc!=2) {
+        printf("Error. Pls supply argument (name of text file).\n");
+        exit(EXIT_FAILURE);
+    }
+
+    aaw_c *aawc=processinpf(argv[1]);
+    prtaawcplain(aawc); // printout original text as well as you can.
+
+    free_aawc(&aawc);
+
+    return 0;
+}
+aaw_c *processinpf(char *fname)
+{
+    /* declarations */
+    FILE *fp=fopen(fname,"r");
+    int i;
+    int numnl=0, oldnumnl=-1;
+    size_t couc /*count chars per line */, couw=0 /* count words */;
+    int c, oldc='\0';
+    boole inword=0;
+    boole intitle=0;
+    boole ona1=1; // on arrow 1 by default must start as 1
+    unsigned lbuf=LBUF /* buffer for number of lines */, cbuf=CBUF /* char buffer for size of w_c's: reused for every word */;
+    aaw_c *aawc=crea_aawc(lbuf); /* array of words per line */
+
+    while( (c=fgetc(fp)) != EOF) {
+        // printf("%c:%i:%i-", (c=='\n')?'X':(c==' ')?'Z':c, inword, intitle);
+        if(c=='\n') {
+            numnl++;
+            c=' ';
+        }
+        if( intitle & ((c ==' ') | (c == '\t') | (c=='=') | (c=='|')) ) {
+            if( inword==1) { /* cue word-ending procedure */
+                printf("%c:CLW ", c);  // close word
+                aawc->aaw[aawc->numl]->aw[couw]->w[couc++]='\0';
+                aawc->aaw[aawc->numl]->aw[couw]->lp1=couc;
+                norm_wc(aawc->aaw[aawc->numl]->aw+couw);
+                couw++; /* verified: this has to be here */
+                inword=0;
+                if(numnl == oldnumnl+1)// therefore, it's the last letter in the last word of an intitle
+                    intitle=0;
+            }
+        } else if((c=='>') & (intitle==0)) { // note this is for closing off the last line, not "starting" it
+            printf("%c:CFL ", c); // close fasta line
+            if(aawc->numl >=lbuf-1) {
+                lbuf += LBUF;
+                aawc->aaw=realloc(aawc->aaw, lbuf*sizeof(aw_c*));
+                for(i=lbuf-LBUF; i<lbuf; ++i)
+                    aawc->aaw[i]=crea_awc(WABUF);
+            }
+            intitle=1;
+            inword=0; // critical line
+            if(ona1==0) {
+                aawc->aaw[aawc->numl]->aw[couw]->w[couc++]='\0';
+                aawc->aaw[aawc->numl]->aw[couw]->lp1=couc;
+                norm_wc(aawc->aaw[aawc->numl]->aw+couw);
+                couw++; /* verified: this has to be here */
+                aawc->aaw[aawc->numl]->al=couw;
+                norm_awc(aawc->aaw+aawc->numl);
+                aawc->numl++;
+                couw=0;
+            } else
+                ona1=0;
+        } else if(inword==0) { /* a normal character opens word */
+            printf("%c:IW0 ", c); 
+            if(couw ==aawc->aaw[aawc->numl]->ab-1) /* new word opening */
+                reall_awc(aawc->aaw+aawc->numl, WABUF);
+            couc=0;
+            cbuf=CBUF;
+            aawc->aaw[aawc->numl]->aw[couw]->w[couc++]=c;
+            inword=1;
+        } else if(inword) { /* simply store */
+            if((c==' ') & !intitle & (numnl == oldnumnl))
+                continue;
+            printf("%c:IW1 ", c); 
+            if(couc == cbuf-1)
+                reall_wc(aawc->aaw[aawc->numl]->aw+couw, &cbuf);
+            aawc->aaw[aawc->numl]->aw[couw]->w[couc++]=c;
+        }
+        //         if(numnl==4)
+        //             break;
+        oldc=c;
+        oldnumnl=numnl;
+        // printf("%c:%i:%zu:%i ", (c=='\n')?'X':(c==' ')?'Z':c, inword, couw, intitle);
+    } /* end of big for statement */
+    /* close off the last line */
+    aawc->aaw[aawc->numl]->aw[couw]->w[couc++]='\0';
+    aawc->aaw[aawc->numl]->aw[couw]->lp1=couc;
+    norm_wc(aawc->aaw[aawc->numl]->aw+couw);
+    couw++; /* verified: this has to be here */
+    aawc->aaw[aawc->numl]->al=couw;
+    norm_awc(aawc->aaw+aawc->numl);
+    aawc->numl++;
+
+    fclose(fp);
+    putchar('\n');
+    putchar('\n');
+
+    /* normalization stage */
+    for(i=aawc->numl; i<lbuf; ++i) {
+        free_awc(aawc->aaw+i);
+    }
+    aawc->aaw=realloc(aawc->aaw, 1+aawc->numl*sizeof(aw_c*));
+
+    return aawc;
+}
